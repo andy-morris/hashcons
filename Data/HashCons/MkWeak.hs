86af1bb2fe25ae7fce312062ca51b0c1940fe7ad
Andy Morris <hello@andy-morris.xyz>
2018-03-01 19:21:34 +0100

    Add MkWeak docs

--- Data/HashCons/MkWeak.hs
+++ Data/HashCons/MkWeak.hs
@@ -25,14 +25,34 @@ import GHC.Weak  (Weak (..), deRefWeak)
 import GHC.Exts  (TYPE, RuntimeRep (..))
 
 
+-- | A finalizer is a procedure attached to a weak pointer, which is run soon
+-- after the weak pointer's key is garbage collected.
 type Finalizer = IO ()
 
+-- | A class for weak pointer keys.
+--
+-- Creating weak pointers is only really safe for certain types of key. Ordinary
+-- Haskell values do not have a well defined lifetime. an example is with
+-- @UNPACK@ pragmas, as in
+-- <https://mail.haskell.org/pipermail/haskell-cafe/2018-January/128513.html>;
+-- in principle the optimiser may also share or duplicate identical values.
+--
+-- This means that a value with an attached finaliser might go out of scope at
+-- an unexpected time, at which point the finalizer will also be run. This might
+-- lead to, e.g., file handles being closed while they are still in use.
+--
+-- In short, this class should only be implemented by values with specific
+-- identity, like references, and the key should be the underlying primitive.
+-- The instance for 'IORef' attaches the pointer to the 'GHC.Prim.MutVar#'
+-- inside, for example.
 class MkWeak k where
   mkWeak :: k -> v -> Maybe Finalizer -> IO (Weak v)
 
+  -- | Make a weak pointer where the value is the key.
   mkWeakPtr :: k -> Maybe Finalizer -> IO (Weak k)
   mkWeakPtr x = mkWeak x x
 
+  -- | Add a finalizer to a value.
   addFinalizer :: k -> Finalizer -> IO ()
   addFinalizer x fin = () <$ mkWeak x () (Just fin)
 
